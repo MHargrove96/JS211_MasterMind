830ca90b5c8b0c3931ca612447132f5bf713e041
MHargrove96 <86787734+MHargrove96@users.noreply.github.com>
2022-01-10 18:43:57 -0600

    Passed all tests

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 'use strict';
 
+
 const assert = require('assert');
 const readline = require('readline');
 const rl = readline.createInterface({
@@ -10,6 +11,8 @@ const rl = readline.createInterface({
 let board = [];
 let solution = '';
 let letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'];
+let hint = ''; 
+
 
 const printBoard = () =>  {
   for (let i = 0; i < board.length; i++) {
@@ -28,15 +31,53 @@ const getRandomInt = (min, max) => {
   return Math.floor(Math.random() * (max - min)) + min;
 }
 
-const generateHint = () =>  {
+const generateHint = (guess) =>  {
   // your code here
+  let solutionArray = solution.split('')
+  let guessArray = guess.split('')
+  let correctLetterLocations = 0; 
+  let correctLetters = 0; 
+  let guessed = guess
+
+  for (let i = 0; i < solutionArray.length; i++) {
+    if(solutionArray[i] === guessArray[i]){
+      correctLetterLocations += 1
+      solutionArray[i] = null
+    }
+  }
+
+  for(let i = 0; i < solutionArray.length; i++) {
+    let targetIndex = solutionArray.indexOf(guessArray[i]);
+    if(targetIndex > -1){
+      correctLetters +=1 
+      solutionArray[targetIndex] = null
+    }
+  }
+
+  hint = `${correctLetterLocations}-${correctLetters}`
+  board.push(guessed)
+  // console.log(board)
+  return `${correctLetterLocations}-${correctLetters}`
 }
 
 const mastermind = (guess) => {
   solution = 'abcd'; // Comment this out to generate a random solution
+  hint = '';
+  console.log('HINT', generateHint(guess)) 
   // your code here
+  if (guess === solution){
+    console.log('You guessed it!')
+    return 'You guessed it!'
+  } 
+  if (board.length === 10) {
+    // console.log('You ran out of turns! The solution was' + solution)
+    return 'You ran out of turns! The solution was' + solution
+  } else {
+    // console.log('Guess again')
+    return 'Guess again.'
+  }
 }
-
+// ------------------- TEST ------------------- TEST ------------------- TEST ------------------- TEST ------------------- TEST ------------------- TEST ------------------- TEST ------------------- TEST ------------------- TEST 
 
 const getPrompt = () =>  {
   rl.question('guess: ', (guess) => {
